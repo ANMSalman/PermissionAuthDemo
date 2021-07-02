bf4331631e7610cc25eaac027a803b91936d78b5
Mohamed Salman <salman.primary@gmail.com>
2021-07-02 11:03:41 +0530

    Add sample custom authorization handler

--- PermissionAuthDemo/Server/Auth/PermissionAuthorizationHandler.cs
+++ PermissionAuthDemo/Server/Auth/PermissionAuthorizationHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using PermissionAuthDemo.Server.Data.Entities;
 using PermissionAuthDemo.Shared.Constants;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,22 +9,45 @@ namespace PermissionAuthDemo.Server.Auth
 {
     internal class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public PermissionAuthorizationHandler(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
         {
+
             if (context.User == null)
             {
-                await Task.CompletedTask;
+                return;
             }
 
-            var permissions = context.User.Claims.Where(x =>
-                                                x.Type == ApplicationClaimTypes.Permission
-                                                && x.Value == requirement.Permission
-                                                && x.Issuer == "LOCAL AUTHORITY");
+            // Get all the roles the user belongs to and check if any of the roles has the permission required
+            // for the authorization to succeed.
+            var user = await _userManager.GetUserAsync(context.User);
+            var userRoleNames = await _userManager.GetRolesAsync(user);
+            var userRoles = _roleManager.Roles.Where(x => userRoleNames.Contains(x.Name));
 
-            if (permissions.Any())
+            foreach (var role in userRoles)
             {
-                context.Succeed(requirement);
-                await Task.CompletedTask;
+                var roleClaims = await _roleManager.GetClaimsAsync(role);
+
+                var permissions = roleClaims
+                    .Where(x => x.Type == ApplicationClaimTypes.Permission
+                                && x.Value == requirement.Permission
+                                && x.Issuer == "LOCAL AUTHORITY")
+                    .Select(x => x.Value);
+
+
+                if (permissions.Any())
+                {
+                    context.Succeed(requirement);
+                    return;
+                }
             }
         }
     }
