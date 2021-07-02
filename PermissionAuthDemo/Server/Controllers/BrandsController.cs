14190f71283125b58db5a720ed391a48cdfe9e78
Mohamed Salman <salman.primary@gmail.com>
2021-07-02 15:54:47 +0530

    Add sample controllers for Brands and Products

---
+++ PermissionAuthDemo/Server/Controllers/BrandsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PermissionAuthDemo.Shared.Constants;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PermissionAuthDemo.Server.Controllers
+{
+    [Route("api/brands")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        // GET: api/brands
+        [Authorize(Policy = Permissions.Brands.View)]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "brand 1", "brand 2" };
+        }
+
+        // GET api/brands/5
+        [Authorize(Policy = Permissions.Brands.View)]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"brand {id}";
+        }
+
+        // POST api/brands
+        [Authorize(Policy = Permissions.Brands.Create)]
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return $"brand created";
+        }
+
+        // PUT api/brands/5
+        [Authorize(Policy = Permissions.Brands.Edit)]
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] string value)
+        {
+            return $"brand updated";
+        }
+
+        // DELETE api/brands/5
+        [Authorize(Policy = Permissions.Brands.Delete)]
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"brand deleted";
+        }
+    }
+}
---
+++ PermissionAuthDemo/Server/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PermissionAuthDemo.Shared.Constants;
+using System.Collections.Generic;
+using System.Threading;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PermissionAuthDemo.Server.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/products
+        [Authorize(Policy = Permissions.Products.View)]
+        [HttpGet]
+        public IEnumerable<string> Get(CancellationToken token)
+        {
+            return new string[] { "product 1", "product 2" };
+        }
+
+        // GET api/products/5
+        [Authorize(Policy = Permissions.Products.View)]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"product {id}";
+        }
+
+        // POST api/products
+        [Authorize(Policy = Permissions.Products.Create)]
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return $"product created";
+        }
+
+        // PUT api/products/5
+        [Authorize(Policy = Permissions.Products.Edit)]
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] string value)
+        {
+            return $"product updated";
+        }
+
+        // DELETE api/products/5
+        [Authorize(Policy = Permissions.Products.Delete)]
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"product deleted";
+        }
+    }
+}
